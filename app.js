a00161d84e3c012d19c9fc6069b00979ca258759
pouria drd <pouriadrd@gmail.com>
2025-10-02 21:12:29 +0330

    added error middleware

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import coolieParser from "cookie-parser";
+import connectToDatabase from "./database/mongodb.js";
 
+const app = express();
+// Routes
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import subscriptionRoutes from "./routes/subscription.routes.js";
 
-import connectToDatabase from "./database/mongodb.js";
-
-const app = express();
-
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/subscriptions", subscriptionRoutes);
+
+// Middlewares
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+app.use(express.json()); // JSON middleware for parsing request body
+app.use(express.urlencoded({ extended: false })); // URL encoded middleware for parsing request body
+app.use(coolieParser()); // Cookie parser middleware for parsing cookies in request
+app.use(errorMiddleware); // Error middleware for handling errors
 
 app.get("/", (req, res) => {
 	res.send("Welcome to Subscription Tracker API");
